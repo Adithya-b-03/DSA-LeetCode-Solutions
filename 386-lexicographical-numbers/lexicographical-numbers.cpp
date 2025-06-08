f7c10551b17d0ed1cf1cde9fdece7de51579b85c
Adithya__... <adithyaakash0304@gmail.com>
2025-06-08 12:08:32 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (83.88%) - LeetSync

---
+++ 386-lexicographical-numbers/lexicographical-numbers.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> lexicalOrder(int n) {
+        vector<int> ans;
+        int x = 1;
+        for(int i = 1 ; i <= n ; i++){
+            ans.push_back(x);
+            if(x*10 <= n){
+                x *= 10;
+                continue;
+            }
+            if(x+1 > n){
+                x /= 10;
+            }
+            x++;
+            while(x%10 == 0){
+                x /= 10;
+            }
+        }
+        return ans;
+    }
+};
