c3d90f72e6fc9971c44a1032922db94ede39cc8a
Adithya__... <adithyaakash0304@gmail.com>
2025-06-11 17:39:42 +0530

    Time: 4 ms (12.65%) | Memory: 44.8 MB (22.45%) - LeetSync

--- 445-add-two-numbers-ii/add-two-numbers-ii.java
+++ 445-add-two-numbers-ii/add-two-numbers-ii.java
@@ -9,37 +9,57 @@
  * }
  */
 class Solution {
+    public ListNode reverseList(ListNode head){
+        if(head == null || head.next == null){
+            return head;
+        }
+        ListNode past =null;
+        ListNode present = head;
+        while(present != null){
+            ListNode future = present.next;
+            present.next = past;
+            past = present;
+            present = future;
+        }
+        return past;
+    }
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode t1 = l1;
+        ListNode t2 = l2; 
+        ListNode dummy = new ListNode(-1);
+        ListNode t = dummy;
         Stack<Integer> s1 = new Stack<>();
         Stack<Integer> s2 = new Stack<>();
-
-        while (l1 != null) {
-            s1.push(l1.val);
-            l1 = l1.next;
+        while(t1 != null){
+            s1.push(t1.val);
+            t1 =t1.next;
         }
-
-        while (l2 != null) {
-            s2.push(l2.val);
-            l2 = l2.next;
+        while(t2 != null){
+            s2.push(t2.val);
+            t2 = t2.next;
         }
-
         int carry = 0;
-        ListNode result = null;
-
-        while (!s1.isEmpty() || !s2.isEmpty() || carry != 0) {
+        while(!s1.isEmpty() || !s2.isEmpty()){
             int sum = carry;
-
-            if (!s1.isEmpty()) sum += s1.pop();
-            if (!s2.isEmpty()) sum += s2.pop();
-
-            // Create a new node and insert at front
-            ListNode node = new ListNode(sum % 10);
-            node.next = result;
-            result = node;
-
-            carry = sum / 10;
+            if(!s1.isEmpty()){
+                sum += s1.peek();
+                s1.pop();
+            }
+            if(!s2.isEmpty()){
+                sum += s2.peek();
+                s2.pop();
+            }
+            ListNode n = new ListNode(sum%10);
+            t.next = n;
+            t = t.next;
+            carry = sum/10;
         }
-
-        return result;
+        if(carry != 0){
+            ListNode n = new ListNode(carry);
+            t.next = n;
+            t = t.next;
+        }
+        ListNode h = reverseList(dummy.next);
+        return h;
     }
 }
