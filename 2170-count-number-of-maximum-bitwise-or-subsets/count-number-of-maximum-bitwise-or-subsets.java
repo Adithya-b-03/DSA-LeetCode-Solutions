c360d15f1f8a66994e185ec33302918e82856f6e
Adithya__... <adithyaakash0304@gmail.com>
2025-07-28 19:30:25 +0530

    Time: 47 ms (27.93%) | Memory: 65.7 MB (5.54%) - LeetSync

--- 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.java
+++ 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.java
@@ -1,21 +1,30 @@
 class Solution {
     int maxOr = 0;
-    public int count(int i, int c, int[] nums){
+    public int count(int i, int c, int[] nums,int dp[][]){
         if(i == nums.length){
             if(c == maxOr){
                 return 1;
             }
             return 0;
         }
-        int t = count(i+1,c|nums[i],nums);
-        int nt = count(i+1,c,nums);
-        return t+nt;
+        if(dp[i][c] != -1){
+            return dp[i][c];
+        }
+        int t = count(i+1,c|nums[i],nums,dp);
+        int nt = count(i+1,c,nums,dp);
+        return dp[i][c] = t+nt;
     }
     public int countMaxOrSubsets(int[] nums) {
         for(int i = 0 ; i < nums.length ; i++){
             maxOr = maxOr | nums[i];
         }
-        int c = count(0,0,nums);
+        int[][] dp = new int[nums.length+1][maxOr+1];
+        for(int i = 0 ; i < nums.length+1 ; i++){
+            for(int j = 0 ; j < maxOr+1 ; j++){
+                dp[i][j] = -1;
+            }
+        }
+        int c = count(0,0,nums,dp);
         return c;
     }
 }
