c6d78db38fb9eb32aff33a043aa6efad32514fb5
Adithya__... <adithyaakash0304@gmail.com>
2025-08-10 23:39:28 +0530

    Time: 4 ms (54.74%) | Memory: 45.5 MB (66.13%) - LeetSync

--- 137-single-number-ii/single-number-ii.java
+++ 137-single-number-ii/single-number-ii.java
@@ -1,17 +1,27 @@
 class Solution {
-    public int singleNumber(int[] nums) {
-        int ans = 0;
-        for(int bitIndex = 0 ; bitIndex < 32 ; bitIndex++){
-            int c = 0;
-            for(int i = 0 ; i < nums.length ; i++){
-                if((nums[i] & (1<<bitIndex)) != 0){
-                    c++;
-                }
-            }
-            if(c%3 != 0){
-                ans = ans | (1<<bitIndex);
+    public int helper(int[] nums){
+        Arrays.sort(nums);
+        for(int i = 1 ; i < nums.length ; i+=3){
+            if(nums[i] != nums[i-1]){
+                return nums[i-1];
             }
         }
-        return ans;
+        return nums[nums.length-1];
+    }
+    public int singleNumber(int[] nums) {
+        // int ans = 0;
+        // for(int bitIndex = 0 ; bitIndex < 32 ; bitIndex++){
+        //     int c = 0;
+        //     for(int i = 0 ; i < nums.length ; i++){
+        //         if((nums[i] & (1<<bitIndex)) != 0){
+        //             c++;
+        //         }
+        //     }
+        //     if(c%3 != 0){
+        //         ans = ans | (1<<bitIndex);
+        //     }
+        // }
+        // return ans;
+        return helper(nums);
     }
 }
