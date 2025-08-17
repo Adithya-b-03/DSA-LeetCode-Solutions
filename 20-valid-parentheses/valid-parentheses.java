22b241a431935a07e0f9fc608a72f3f3741aa634
Adithya__... <adithyaakash0304@gmail.com>
2025-08-17 15:52:40 +0530

    Time: 2 ms (96.55%) | Memory: 41.5 MB (92.66%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for(int i = 0 ; i < s.length() ; i++){
+            char ch = s.charAt(i);
+            if(ch=='('||ch=='{'||ch=='['){
+                st.push(ch);
+            }
+            else{
+                if(st.isEmpty()==true){
+                    return false;
+                }
+                char curr = st.pop();
+                if((ch==')' && curr=='(')||(ch=='}' && curr=='{')||(ch==']' && curr=='[')){
+                    continue;
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        return st.isEmpty();
+
+    }
+}
